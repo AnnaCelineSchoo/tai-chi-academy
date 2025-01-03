b9ba3123a7e9393e8200f6a874c417421802efd9
AnnaCelineSchoo <131945624+AnnaCelineSchoo@users.noreply.github.com>
2025-01-03 07:48:54 +0100

    fix routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout.jsx";
 import HomePage from "./pages/HomePage.jsx";
 import TarievenPage from "./pages/TarievenPage.jsx";
@@ -9,6 +9,9 @@ import MediaItemPage from "./pages/MediaItemPage.jsx";
 import ScrollToTop from "./layouts/ScrollToTop.jsx";
 import AgendaPage from "./pages/AgendaPage.jsx";
 
+const isDevelopment = import.meta.env.DEV; // Check if in development mode
+const basename = isDevelopment ? "/" : "/tai-chi-academy/";
+
 // todo:
 // achtergrond contact niet goed
 // maak agenda met lestijden en eventuele wokshops die gepland zijn
@@ -16,18 +19,20 @@ import AgendaPage from "./pages/AgendaPage.jsx";
 // add some security for bots in the form upload
 
 function App() {
+  console.log("Environment:", import.meta.env);
+  console.log("Using basename:", basename);
   return (
-    <Router>
+    <Router basename={basename}>
       <ScrollToTop />
       <Routes>
         <Route path="/" element={<MainLayout />}>
           <Route index element={<HomePage />} />
-          <Route path="/lessen" element={<LessenPage />} />
-          <Route path="/media" element={<MediaPage />} />
-          <Route path="/tarieven" element={<TarievenPage />} />
-          <Route path="/contact" element={<ContactPage />} />
-          <Route path="/mediaItem/:id" element={<MediaItemPage />} />
-          <Route path="/agenda" element={<AgendaPage />} />
+          <Route path="lessen" element={<LessenPage />} />
+          <Route path="media" element={<MediaPage />} />
+          <Route path="tarieven" element={<TarievenPage />} />
+          <Route path="contact" element={<ContactPage />} />
+          <Route path="mediaItem/:id" element={<MediaItemPage />} />
+          <Route path="agenda" element={<AgendaPage />} />
         </Route>
       </Routes>
     </Router>
