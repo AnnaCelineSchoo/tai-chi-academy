450a00d281ec33ae0cbc4631330647f07639c186
AnnaCelineSchoo <131945624+AnnaCelineSchoo@users.noreply.github.com>
2025-01-03 09:54:07 +0100

    update vindbaarheid contact page

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,10 +1,65 @@
+import { HelmetProvider, Helmet } from "react-helmet-async";
 import Contact from "../components/Contact";
 
 function ContactPage() {
   return (
-    <div>
+    <HelmetProvider>
+      <Helmet>
+        {/* Title for SEO */}
+        <title>Contact - Taijiquan Academy</title>
+
+        {/* Meta Description */}
+        <meta
+          name="description"
+          content="Neem contact op met Taijiquan Academy in Apeldoorn voor vragen over Tai Chi, Qi Gong, Meditatie en zelfverdediging lessen, workshops, of algemene informatie. Wij staan klaar om te helpen!"
+        />
+
+        {/* Keywords (adjust based on your contact and services) */}
+        <meta
+          name="keywords"
+          content="contact, Taijiquan, Taijiquan Academy, Tai Chi, Tai Chi Apeldoorn, Apeldoorn, Tai Chi, lessen, workshops, contactformulier, Qi Gong, Meditatie, zelfverdediging lessen"
+        />
+
+        {/* Canonical Link */}
+        <link rel="canonical" href="https://www.taijiquanacademy.nl/contact" />
+
+        {/* Open Graph Tags for Social Media Sharing */}
+        <meta
+          property="og:title"
+          content="Contact - Taijiquan Academy -  Tai Chi Academie"
+        />
+        <meta
+          property="og:description"
+          content="Neem contact op met Taijiquan Academy in Apeldoorn voor vragen over Tai Chi, Qi Gong, Meditatie en zelfverdediging lessen, workshops, of algemene informatie. Wij staan klaar om te helpen!"
+        />
+        <meta
+          property="og:url"
+          content="https://www.taijiquanacademy.nl/contact"
+        />
+        <meta property="og:type" content="website" />
+
+        {/* Structured Data (JSON-LD) for Search Engines */}
+        <script type="application/ld+json">
+          {`
+          {
+            "@context": "https://schema.org",
+            "@type": "WebPage",
+            "name": "Contact - Taijiquan Academy",
+            "url": "https://www.taijiquanacademy.nl/contact",
+            "description": "Neem contact op met Taijiquan Academy in Apeldoorn voor vragen over Tai Chi, Qi Gong, Meditatie en zelfverdediging lessen, workshops, of algemene informatie. Wij staan klaar om te helpen!",
+            "mainEntityOfPage": {
+              "@type": "WebPage",
+              "@id": "https://www.taijiquanacademy.nl/contact"
+            }
+          }
+          `}
+        </script>
+      </Helmet>
+
+      {/* Contact Component */}
       <Contact />
-    </div>
+    </HelmetProvider>
   );
 }
+
 export default ContactPage;
